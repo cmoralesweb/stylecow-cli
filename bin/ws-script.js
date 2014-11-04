3061c2b618767ccabc7b54b87d3f275c164a7071
oscarotero <oom@oscarotero.com>
2014-11-05 00:22:34 +0100

    websocket/watch fixes

--- bin/ws-script.js
+++ bin/ws-script.js
@@ -1,24 +1,21 @@
 (function () {
-  var ws, style = document.createElement('style'),link = document.querySelector('link[rel="stylesheet"]');
+  var ws,
+    style = document.createElement('style'),
+    link = document.querySelector('link[rel="stylesheet"]');
   document.head.appendChild(style);
 
   function socket () {
-    ws = new WebSocket('ws://127.0.0.1:8080');
-    ws.onmessage = function (e) {
-      style.innerHTML = e.data;
-      if (link) {
-        link.parentNode.removeChild(link);
-        link = null;
+    if (!ws || ws.readyState !== 1) {
+      ws = new WebSocket('ws://127.0.0.1:8080');
+      ws.onmessage = function (e) {
+        style.innerHTML = e.data;
+        if (link) {
+          link.parentNode.removeChild(link);
+          link = null;
+        }
       }
     }
   }
-  setInterval(function () {
-    if (ws) {
-      if (ws.readyState !==1) {
-        socket();
-      }
-    } else {
-      socket();
-    }
-  }, 1000);
+
+  setInterval(socket, 1000);
 })();
